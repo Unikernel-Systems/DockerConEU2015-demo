68cb78e91f7a9a99dffa6ccd228feccab1837d0b
Martin Lucina <mato@unikernel.com>
2015-11-20 14:11:29 +0100

    Cosmetics: Make the nginx and php landing pages nicer

--- php/fs/data/www/index.php
+++ php/fs/data/www/index.php
@@ -1,18 +1,31 @@
 <?php 
 date_default_timezone_set('UTC');
 ?>
-<html>
-<body style="font-size: 14pt;">
-    <img src="logo150.png"/>
+<head>
+  <title>Unikernels, meet Docker!</title>
+  <style type="text/css">
+      body { font-size: 14pt; }
+      img { vertical-align: middle; }
+  </style>
+</head>
+<body>
+  <p>
     Hello, <?php echo $_SERVER['REMOTE_ADDR']; ?>
-    <p>
+  </p>
+  <p>
     It is now <?php echo date(DATE_RFC2822); ?>
-    <p>
-    Served to you by
-    <a href="http://nginx.org/">Nginx</a>,
-    running on a
-    <a href="http://rumpkernel.org">rump kernel</a>...
-    <p>
+  </p>
+  <p>
+    Served to you by <a href="http://nginx.org/">Nginx</a>.
+  </p>
+  <p>
+    Powered by <a href="http://rumpkernel.org/">Rump Kernels</a>
+    <img src="logo150.png" width="40px" height="40px"/>
+    and <a href="http://www.docker.com/">Docker</a>
+    <img src="small_h.png" height="40px"/>.
+  </p>
+  <p>
     Try <a href="phpinfo.php">phpinfo()</a>.
+  </p>
 </body>
 </html>
